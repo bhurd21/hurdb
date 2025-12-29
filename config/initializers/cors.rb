48ca67ff58b4a5e6233a121f288c36ae60455e62
Brennan Hurd <brennan@Brennans-MacBook-Air.local>
2025-12-29 14:02:59 -0600

    update cors to new immaculate grid url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     allowed_origins = [
-      'https://www.immaculategrid.com'
+      'https://www.immaculategrid.com',
+      'https://www.sports-reference.com',
     ]
 
     origins(*allowed_origins)
